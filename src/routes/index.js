78a5838dc5fe7789a08fa148e61b3889ccce6801
medvar <medvar9@gmail.com>
2019-10-23 22:49:01 -0600

    version funcional

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,10 @@ const router = Router();
 const fs = require('fs');
 const uuid = require('uuid/v4');
 
-const json_pacientes = fs.readFileSync('src/pacientes.json', 'utf-8');
+let json_pacientes = fs.readFileSync('src/pacientes.json', 'utf-8');
 const pacientes = JSON.parse(json_pacientes);
-const json_examenes = fs.readFileSync('src/examen.json', 'utf-8');
+let json_examenes = fs.readFileSync('src/examen.json', 'utf-8');
 const examenes = JSON.parse(json_examenes);
-
-let idp = "";
 
 router.get('/', (req, res) => {
     res.render('index', {
@@ -21,15 +19,47 @@ router.get('/paciente', (req, res) => {
 });
 
 router.get('/examen/:id', (req, res) => {
-    //console.log(req.params);
-    idp = req.params.id;
+    let idp = req.params.id;
     res.render('examen', {
         pacientes,
         idp
     });
 });
 
-router.post('/Paciente', (req, res) => {
+
+
+
+router.get('/examenes/:id', (req, res) => {
+    let idp = req.params.id;
+    res.render('examenes', {
+        pacientes,
+        examenes,
+        idp
+    });
+});
+
+router.post('/examen', (req, res) => {
+    let { IdPaciente, NivelEzfuerzo, Posicion, Fecha, Nota, Latidos, Diagnostico } = req.body;
+    let nuevoExamen = {
+        id: uuid(),
+        IdPaciente,
+        NivelEzfuerzo,
+        Posicion,
+        Fecha: Fecha,
+        Nota: Nota.trim(),
+        Latidos,
+        Diagnostico
+    };
+    examenes.push(nuevoExamen);
+
+    json_examenes = JSON.stringify(examenes);
+    fs.writeFileSync('src/examen.json', json_examenes, 'utf-8');
+
+    res.redirect('/examenes/' + IdPaciente);
+});
+
+router.post('/paciente', (req, res) => {
+    console.log(req.body);
     const { Nombre, Apellido, Edad, Sexo, Fuma, HorasEjercicio } = req.body;
     let newPaciente = {
         id: uuid(),
@@ -47,7 +77,7 @@ router.post('/Paciente', (req, res) => {
 
     pacientes.push(newPaciente);
 
-    const json_pacientes = JSON.stringify(pacientes);
+    json_pacientes = JSON.stringify(pacientes);
     fs.writeFileSync('src/pacientes.json', json_pacientes, 'utf-8');
 
     res.redirect('/');
